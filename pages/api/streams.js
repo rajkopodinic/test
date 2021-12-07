0c207abb0ce444322161ace7765e2362582fae17
Rajko Podinic <rajkopodinic@192-168-1-7.tpgi.com.au>
2021-12-07 21:54:06 +1100

    update

--- pages/api/streams.js
+++ pages/api/streams.js
@@ -1,4 +1,22 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import { onSnapshot } from "@firebase/firestore";
+// import { collectionRefStreams } from "../../lib/firebase";
+
+// var streams = [];
+
+// (function streamsData() {
+//   onSnapshot((collectionRefStreams), (snapshot) => {
+//     snapshot.docs.forEach((doc) => {
+//       streams.push({ ...doc.data(), firebaseId: doc.id })
+//     })
+//   })
+// })();
+
+// export default function streamDataAPI(req, res) {
+//   // res.status(200).json({ name: 'John Doe' })
+//   res.status(200).json(streams)
+// }
+
 import { onSnapshot } from "@firebase/firestore";
 import { collectionRefStreams } from "../../lib/firebase";
 
@@ -6,9 +24,11 @@ var streams = [];
 
 (function streamsData() {
   onSnapshot((collectionRefStreams), (snapshot) => {
+    var updatedStreams = []
     snapshot.docs.forEach((doc) => {
-      streams.push({ ...doc.data(), firebaseId: doc.id })
+      updatedStreams.push({ ...doc.data(), firebaseId: doc.id })
     })
+    streams = updatedStreams;
   })
 })();
 
